d22796dead6879d3914917325f5751ea9456017c
Hayat ilyas <ihayat855@gmail.com>
2023-04-28 09:05:39 +0530

    Adding user modal.

---
+++ Apps/Auth/User/Models/UserModal.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        max: 20,
+        min: 4
+    },
+    lastName:{
+        type:String,
+        trim:true,
+        max:20,
+        min:5
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 20,
+        min: 8
+    },
+    is_active: {
+        type: Boolean,
+        default: true
+    },
+    Phone_Number:{
+        type:Number,
+        max:10,
+        min:10
+    }
+},
+    {
+        timestamps: {
+            createdAt: 'created_at', 
+            updatedAt: 'updated_at' 
+        }
+    }
+)
+
+
+const UserModal = new mongoose.model('user',UserSchema)
+
+export default UserModal ;
