e7154330a6af3f1d306c383c50c330375db24067
Hayat ilyas <ihayat855@gmail.com>
2023-04-28 09:04:08 +0530

    Adding user router

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import ErrorMiddleware  from "./Middleware/ErrorMiddleware.js";
 import ConnectDb from "./Configuration/ConnectDb.js";
+import UserRouter from "./Apps/Auth/User/Routes/UserRoutes.js";
 
 // configuration for the server
 const app = express();
 config();
 app.use(express.json())
 app.use(cookieParser());
+
+
+// All App Routes
+app.use(UserRouter)
 app.use(ErrorMiddleware)
+
 
 // first connect to  the database then open the server port
 ConnectDb().then(()=>{
